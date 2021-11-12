b4fd91247e475e85971ddd209fda76b38756d97a
bernhard.strobl <bernhard.s.strobl@gmail.com>
2021-11-12 20:03:14 +0100

    Initial commit HashMap

---
+++ src/at/campus02/strobl/mapdemo/MapDemo.java
@@ -0,0 +1,13 @@
+package at.campus02.strobl.mapdemo;
+
+import java.util.HashMap;
+
+public class MapDemo {
+
+    public static void main(String[] args) {
+
+        HashMap<String, Integer> gradeOfStudents = new HashMap<>();
+
+        
+    }
+}
