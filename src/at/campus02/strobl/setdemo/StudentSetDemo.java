fa0f0543d037e5cfee69809cf5b10fec5c5568cc
bernhard.strobl <bernhard.s.strobl@gmail.com>
2021-11-12 19:36:28 +0100

    HashSet Testfälle ausprogrammiert; Objekt .add, .remove; for Schleife zum Einzelausweis

--- src/at/campus02/strobl/setdemo/StudentSetDemo.java
+++ src/at/campus02/strobl/setdemo/StudentSetDemo.java
@@ -8,6 +8,27 @@ public class StudentSetDemo {
 
         HashSet<String> students = new HashSet<>();
 
-        
+        students.add("Max Mustermann");
+        students.add("Susi Sorglos");
+        String maxi = "Maxi Testfall";
+        students.add(maxi);
+
+        System.out.println(students);
+
+        students.remove(maxi);
+        System.out.println("students = " +students);
+
+        boolean isMaxiInSet = students.contains(maxi);
+        boolean isMaxInSet = students.contains("Max Mustermann");
+        System.out.println("Is Maxi in Set = " +isMaxiInSet);
+        System.out.println("Is Max in Set = " +isMaxInSet);
+
+        students.add(maxi);
+
+        for (String student : students) {
+            System.out.println("Student = " +student);
+        }
+
+
     }
 }
