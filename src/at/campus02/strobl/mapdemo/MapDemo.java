3192828ebdd30f641428b803e154767acd9b3aca
bernhard.strobl <bernhard.s.strobl@gmail.com>
2021-11-12 20:12:55 +0100

    HashMap Demo komplett

--- src/at/campus02/strobl/mapdemo/MapDemo.java
+++ src/at/campus02/strobl/mapdemo/MapDemo.java
@@ -1,6 +1,8 @@
 package at.campus02.strobl.mapdemo;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MapDemo {
 
@@ -8,6 +10,22 @@ public class MapDemo {
 
         HashMap<String, Integer> gradeOfStudents = new HashMap<>();
 
+        gradeOfStudents.put("Mustermann", 1);
+        gradeOfStudents.put("Musterfrau", 2);
+        System.out.println("gradeOfStudents = " + gradeOfStudents);
         
+        gradeOfStudents.put("Mustermann", 3);
+        System.out.println("gradeOfStudents = " + gradeOfStudents);
+
+        Set<String> students = gradeOfStudents.keySet();
+        for (String student : students) {
+            Integer grade = gradeOfStudents.get(student);
+            System.out.println("Student " + student+ " hat die Note " +grade);
+
+        }
+
+        System.out.println(" ");
+        Integer gradeOfMustermann = gradeOfStudents.get("Mustermann");
+        System.out.println("gradeOfMustermann = " + gradeOfMustermann);
     }
 }
